a54365013bb443807cf8b4c26760535876cbc8e0
E.N <67209588+ElisNz@users.noreply.github.com>
2024-11-26 10:46:36 +0100

    fixed import issue

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,7 +1,6 @@
 import { Wheel } from './Wheel';
 import { Globe } from './Globe';
-import { GLtest } from './GLtest';
-import { Rotation } from './CanvasRotation';
+import { CanvasRotation } from './CanvasRotation';
 import { Header } from './Header';
 
-export { Globe, Wheel, GLtest, Rotation, Header };
+export { Globe, Wheel, CanvasRotation, Header };
