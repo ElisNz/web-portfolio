8c55da541d3a53a80383e996b283b520d8aebe35
E.N <67209588+ElisNz@users.noreply.github.com>
2024-12-03 21:34:32 +0100

    added text objects to the scene that track the rotation of the canvas

--- src/app/components/CanvasRotationScene/index.tsx
+++ src/app/components/CanvasRotationScene/index.tsx
@@ -9,6 +9,8 @@ import { MTLLoader } from 'three/addons/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { label } from 'three/webgpu';
+import { text } from 'stream/consumers';
 
 
 const CameraController = ({orbref, autoRotate, clickedObj, isTracking, overview, cover}) => {
@@ -19,19 +21,13 @@ const CameraController = ({orbref, autoRotate, clickedObj, isTracking, overview,
   controls.autoRotateSpeed = 0.1;
 
 
-/*   controls.enableRotate = false;
+  controls.enableRotate = false;
   controls.enablePan = false;
-  controls.enableZoom = false; */
+  controls.enableZoom = false;
   controls.enableDamping = true;
   controls.screenSpacePanning = false;
   controls.zoomToCursor = true;
 
-
-  const startY = controls.object.position.y;
-  const startZ = controls.object.position.z;
-  let zTarget = 4;
-  let xTarget = 0;
-  let yTarget = 0;
 
   useEffect(
      () => {
@@ -67,7 +63,7 @@ const CameraController = ({orbref, autoRotate, clickedObj, isTracking, overview,
     }
   }, [isTracking]);
 
-  useFrame((state, delta) => {
+  useFrame(() => {
 
     // initial pan to object
     if(overview && !cover) {
@@ -76,12 +72,11 @@ const CameraController = ({orbref, autoRotate, clickedObj, isTracking, overview,
     }
     // interactive click-pan
     if(isTracking && !overview) {
-      controls.object.position.lerp(new Vector3(-clickedObj.x / 4, 0, -clickedObj.z / 4).round(), 0.008);
-      controls.target.lerp(new Vector3(clickedObj.x / 4, 0, clickedObj.z / 4).round(), 0.008);
+      controls.object.position.lerp(new Vector3(-clickedObj.x / 4, 0, -clickedObj.z / 4), 0.008);
+      controls.target.lerp(new Vector3(clickedObj.x / 4, 0, clickedObj.z / 4), 0.008);
       controls.cursor.addVectors(controls.target, controls.object.position);
     }
 
-    
     orbref.current.innerHTML = `<h3>X: ${controls.object.position.x.toFixed(3)}</h3>
                                 <h3>Y: ${controls.object.position.y.toFixed(3)} </h3>
                                 <h3>Z: ${controls.object.position.z.toFixed(3)}</h3>`;
@@ -132,7 +127,10 @@ export const Cactus = (props) => {
 export const Cactus_free = (props) => {
   const [hovered, hover] = useState(false);
   const [loaded, setLoaded] = useState(false);
-  const mesh = useRef(null);
+
+  const textVector = new Vector3(0, 0, 0);
+  const meshRef = useRef(null);
+
   const hoveredRotationSpeed = 0.2;
 
 /*   const materials = useLoader(MTLLoader, ['/models/cactus_free/10436_Cactus_v1_max2010_it2.mtl']); */
@@ -141,6 +139,15 @@ export const Cactus_free = (props) => {
     loader.setMaterials(materials[0]); */
   });
 
+ useEffect(() => {
+    let divElem = document.createElement('div');
+    divElem.id = (props.indexNr).toString();
+    divElem.style.position = 'absolute';
+    divElem.style.color = 'white';
+    divElem.innerHTML = `${props.indexNr}`;
+    document.body.appendChild(divElem);
+  }, []);
+  
 
 
   // Change color of the model by traversing meshes and assigning a random color
@@ -151,25 +158,56 @@ export const Cactus_free = (props) => {
       }
     });
     setLoaded(true);
+
   }, [model]);
 
 
   useFrame((state, delta) => {
-    mesh.current.rotation.z += delta * 0.01;
+    
+    meshRef.current.rotation.z += delta * 0.01;
     if (hovered) {
-      mesh.current.rotation.z += delta * hoveredRotationSpeed;
+      meshRef.current.rotation.z += delta * hoveredRotationSpeed;
     }
+
+    const textElement = document.getElementById((props.indexNr).toString());
+
+    if (!textElement) {
+      return;
+    }
+    meshRef.current.updateMatrixWorld();
+    textVector.setFromMatrixPosition(meshRef.current.matrixWorld);
+    textVector.project(state.camera);
+
+    const widthHalf = state.size.width / 2;
+    const heightHalf = state.size.height / 2;
+    const allowedWidth = state.size.width - 50;
+    const allowedHeight = state.size.height - 50;
+
+    textVector.x = (textVector.x * widthHalf) + widthHalf;
+    textVector.y = -(textVector.y * heightHalf) + heightHalf;
+
+    if (textVector.x > allowedWidth || textVector.x < 0 || textVector.y > allowedHeight || textVector.y < 0 || textVector.z < 0 || textVector.z > 1) {
+      textElement.style.display = 'none';
+    } else {
+      textElement.style.left = `${textVector.x % state.size.width}px`;
+      textElement.style.top = `${textVector.y % state.size.height}px`;
+      textElement.style.display = 'block';
+    }
+
   });
 
+
   return (
-    <group {...props} ref={mesh}>
-      <primitive object={model.clone()} scale={hovered ? 1.2 : 1} />
-      <meshLambertMaterial color={'red'} />
-      <mesh position={[0, 0, 150]} onPointerOver={(event) => hover(true)} onPointerOut={(event) => hover(false)} onClick={() => {props.setClickedObj(mesh.current.position); props.setTracking(true); props.setOverview(false); }}>
-        <boxGeometry args={[100, 100, 350]} />
-        <meshPhongMaterial transparent opacity={0} />
+    
+      <mesh {...props} ref={meshRef}>
+        <primitive object={model.clone()} scale={hovered ? 1.2 : 1} />
+        <meshLambertMaterial color={'red'} />
+        <mesh position={[0, 0, 150]} onPointerOver={(event) => hover(true)} onPointerOut={(event) => hover(false)} onClick={() => {props.setClickedObj(meshRef.current.position); props.setTracking(true); props.setOverview(false); }}>
+          <boxGeometry args={[100, 100, 350]} />
+          <meshPhongMaterial transparent opacity={0} />
+        </mesh>
       </mesh>
-    </group>
+
   );
 };
 
@@ -192,13 +230,9 @@ const Rotation = ({setClickedObj, setTracking, cover, setOverview}) => {
           const baseline = i[0] * (-2) - 1.5;
           
           return (
-              <Cactus_free key={index} position={[Math.sin(angle) * radius / 2, baseline, Math.cos(angle) * radius / 2]} scale={i[0] / 20} rotation={[-1.5, 0, 0]} setClickedObj={setClickedObj} setTracking={setTracking} setOverview={setOverview} />
+              <Cactus_free indexNr={index} key={index} position={[Math.sin(angle) * radius / 2, baseline, Math.cos(angle) * radius / 2]} scale={i[0] / 20} rotation={[-1.5, 0, 0]} setClickedObj={setClickedObj} setTracking={setTracking} setOverview={setOverview} />
           )}
         )}
-        <mesh position={[0, 10, 0]}>
-          <boxGeometry args={[100, 100, 100]} />
-          <meshPhongMaterial color={'red'} />
-        </mesh>
     </>
   );
 };
@@ -206,14 +240,9 @@ const Rotation = ({setClickedObj, setTracking, cover, setOverview}) => {
 export const CanvasRotationScene = ({cover, overview, setOverview}) => {
   const [clickedObj, setClickedObj] = useState(new Vector3(0, 0, 4));
   const [isTracking, setTracking] = useState(false);
-
   const trackerRef = useRef(null);
-  const textRef = useRef(null);
+  const canvasRef = useRef(null);
 
-
-  useEffect(() => {
-    console.log(clickedObj);
-  }, [clickedObj]);
 
   useEffect(() => {
     if (cover) {
@@ -225,10 +254,9 @@ export const CanvasRotationScene = ({cover, overview, setOverview}) => {
   return (
     <div className={`fixed w-full h-full ${cover ? '' : 'bg-[coral]/50'}`}>
 
-      <Canvas>
+      <Canvas ref={canvasRef}>
         <CameraController orbref={trackerRef} autoRotate={cover} clickedObj={clickedObj} isTracking={isTracking} overview={overview} cover={cover} />
         
-
         <fog attach="fog" args={['#ffffff', 8, 25]} />
 
         {!cover &&
@@ -241,8 +269,7 @@ export const CanvasRotationScene = ({cover, overview, setOverview}) => {
         </Suspense>
         
       </Canvas>
-      
-      <div ref={textRef} />
+
       <div ref={trackerRef} className='fixed bottom-4 left-0 text-2xl text-[black]/40 p-2' />
     </div>
   );
