ebb7f86e621ced5be52f069194c37add3b37dc74
E.N <67209588+ElisNz@users.noreply.github.com>
2025-04-02 11:15:36 +0200

    specified styling for tablets

--- src/app/screens/MobileProjectScreen/index.tsx
+++ src/app/screens/MobileProjectScreen/index.tsx
@@ -1,8 +1,42 @@
 import { useStore } from "@/app/Store";
+import Image from "next/image";
+
+const projects = [
+  {
+    title: "Project 1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/images/texture_text_test.png",
+  },
+  {
+    title: "Project 2",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/images/texture_text_test.png",
+  },
+  {
+    title: "Project 3",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/images/texture_text_test.png",
+  },
+  {
+    title: "Project 4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "/images/texture_text_test.png",
+  },
+];
 
 export default function MobileProjectScreen() {
   const store = useStore((state) => state);
-  const { scene } = store;
+  const { scene, setScene } = store;
+
+  const ProjectCard = ({ title, description, image }) => (
+    <button type="button" title={title} className="flex flex-col text-center items-center" onClick={() => setScene(title)}>
+      <div className="h-20 w-20 bg-[white]/20">
+        <Image src={image} alt={title} width={100} height={100} />
+      </div>
+      <h2 className="pt-2">{title}</h2>
+    </button>
+  );
+
 
   let backgroundStyle = "";
 
@@ -25,26 +59,9 @@ export default function MobileProjectScreen() {
       className={`fixed w-full h-full ${backgroundStyle} ${scene === "cover" ? "opacity-0" : "opacity-100"}`}
     >
       <div className="h-full flex flex-wrap items-center justify-evenly py-[15vh] gap-4 p-md">
-        <div className="h-20 w-20 bg-[white] text-center">
-          <div className="h-20 w-20 bg-[white]"></div>
-          <h2 className="pt-2">Name_name</h2>
-        </div>
-        <div className="h-20 w-20 bg-[white] text-center">
-          <div className="h-20 w-20 bg-[white]"></div>
-          <h2 className="pt-2">Name_name</h2>
-        </div>
-        <div className="h-20 w-20 bg-[white] text-center">
-          <div className="h-20 w-20 bg-[white]"></div>
-          <h2 className="pt-2">Name_name</h2>
-        </div>
-        <div className="h-20 w-20 bg-[white] text-center">
-          <div className="h-20 w-20 bg-[white]"></div>
-          <h2 className="pt-2">Name_name</h2>
-        </div>
-        <div className="h-20 w-20 bg-[white] text-center">
-          <div className="h-20 w-20 bg-[white]"></div>
-          <h2 className="pt-2">Name_name</h2>
-        </div>
+        {projects.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
       </div>
     </div>
   );
