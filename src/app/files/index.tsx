b89888bcc450efa76ea353e1d485368a1cfba458
E.N <67209588+ElisNz@users.noreply.github.com>
2025-07-01 21:37:34 +0200

    fixed reading of files in src/app/files/index.tsx

--- src/app/files/index.tsx
+++ src/app/files/index.tsx
@@ -1,11 +1,16 @@
 export const getDataUtility = (fileName: string) => {
+  let files: object[];
+  
   switch (process.env.NODE_ENV) {
     case 'development':
-      const files = require(`./${fileName}_dev.json`);
-      return files;
+      files = require(`./${fileName}_dev.json`);
+      break;
     case 'production':
-      return import(`./${fileName}.json`);
+      files = require(`./${fileName}.json`);
+      break;
     default:
       throw new Error('Unknown environment');
   }
+
+  return files;
 };
